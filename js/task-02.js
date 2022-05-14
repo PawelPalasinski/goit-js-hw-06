67f481186f234a9882629efd2420da2eda3a8fbc
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-14 14:56:47 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i++) {
+  let listItem = document.createElement("li");
+  listItem.classList.add('item');
+  listItem.innerHTML = ingredients[i];
+  list.appendChild(listItem);
+}
