397d402c9a160bf3c080e2c035d1d8e3152a9696
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-14 14:17:31 +0200

    1.1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,18 @@
-
-
-const cl = x => console.log(x);
+const cl = (x) => console.log(x);
 
 // Task 1
 
-const listCatArr = Array.from(document.querySelectorAll('#categories>li'));
+const listCatArr = Array.from(document.querySelectorAll("#categories>li"));
 
-cl('Number of categories: ' + listCatArr.length);
+cl("Number of categories: " + listCatArr.length);
+
+const list = document.querySelector(".item");
+const listTitles = document.querySelectorAll(".item>h2");
+const listsInsideList = document.querySelectorAll(".item>ul");
+
+for (let i = 0; i < listTitles.length; i++) {
+  let t = listTitles[i].textContent;
+  let listArr = listsInsideList[i].querySelectorAll("li");
+  let listLen = listArr.length;
+  cl("Category: " + t + "\n" + "Elements: " + listLen);
+}
