2aa920ebe1a36927e261b0755e432c0e00a3fb49
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-15 14:06:01 +0200

    6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector("#validation-input");
+
+textInput.onblur = function () {
+    const textInputLen = textInput.value.length;
+    if (textInputLen === 6) {
+        this.classList.remove("invalid");
+        this.classList.add("valid");
+    } else {
+        this.classList.remove("valid");
+        this.classList.add("invalid");
+    }
+}
