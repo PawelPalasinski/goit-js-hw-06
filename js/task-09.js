459fdf82dbca04e682db6538fcd4b70a0de80752
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-15 17:48:17 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorValSpan = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValSpan.textContent = getRandomHexColor();
+});
