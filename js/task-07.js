db028fc40014b2883901c7b13cefce61dbf64901
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-15 14:18:46 +0200

    7

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+slider.addEventListener("input", onChangeFontStyleInput);
+
+function onChangeFontStyleInput(e) {
+  text.style.fontSize = e.currentTarget.value + "px";
+}
